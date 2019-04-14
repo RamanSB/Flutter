16f23be41ee7e0bc9238ddce486bf1dcb499aa5f
RamanSB <ramansb100@gmail.com>
2019-04-15 00:02:33 +0100

    Using ListView.Builder instead of Column - allows scrollability + loads list items lazily (when required)

--- flutter_course/lib/products.dart
+++ flutter_course/lib/products.dart
@@ -12,20 +12,33 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   } //Syntactic sugar, initializes products
 
+
+  Widget _buildProductList(){
+    Widget productCard = Center(child: new Text("No products found, please add some."));
+    if(products.length > 0){
+      productCard = ListView.builder(
+          itemBuilder: _itemBuilder,
+          itemCount: products.length
+      );
+    }
+    return productCard;
+  }
+
+
+  Widget _itemBuilder(BuildContext context, int index){
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return Column(
-      children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(element)
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    return _buildProductList();
   }
 }
