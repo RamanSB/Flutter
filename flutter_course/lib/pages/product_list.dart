ea1f48385b8c408867a4d97bcf1f7b750156f0a6
RamanSB <ramansb100@gmail.com>
2019-06-03 21:35:14 +0100

    Section 10 complete, minor improvements

--- flutter_course/lib/pages/product_list.dart
+++ flutter_course/lib/pages/product_list.dart
@@ -4,33 +4,56 @@ import './product_edit.dart';
 class ProductListPage extends StatelessWidget {
   List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
+
+  Widget _buildEditProductButton(BuildContext context, int index) {
+    return IconButton(
+      icon: Icon(Icons.mode_edit),
+      onPressed: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (BuildContext context) {
+          return ProductEditPage(
+              product: products[index],
+              updateProduct: updateProduct,
+              productIndex: index);
+        }));
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-          child: ListTile(
-            leading: Image.asset(products[index]['image']),
-            title: Text(products[index]['title'],
-                style: TextStyle(
-                    fontSize: 18.0,
-                    letterSpacing: 3.0,
-                    fontWeight: FontWeight.bold)),
-            trailing: IconButton(
-              icon: Icon(Icons.mode_edit),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (BuildContext context) {
-                  return ProductEditPage(product: products[index], updateProduct: updateProduct, productIndex: index);
-                }));
-              },
-            ),
-          ),
-        );
+        return Dismissible(
+            onDismissed: (DismissDirection direction) {
+              print(direction);
+              if (direction == DismissDirection.endToStart ||
+                  direction == DismissDirection.startToEnd) {
+                deleteProduct(index);
+              }
+            },
+            key: Key(products[index]['title']),
+            background: Container(
+                decoration: BoxDecoration(
+                    gradient: new LinearGradient(
+                        colors: [Colors.red[100], Colors.red]))),
+            child: Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                      backgroundImage: AssetImage(products[index]['image'])),
+                  title: Text(products[index]['title'],
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  subtitle: Text("\$${products[index]['price'].toString()}"),
+                  trailing: _buildEditProductButton(context, index),
+                ),
+              ),
+              Divider()
+            ]));
       },
       itemCount: products.length,
     );
